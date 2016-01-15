4db79013b4c87bb6130d359babbd6a1eb446a144
Lemaitre Cedric <lemaitrecedric@MacBook-Pro-de-Lemaitre.local>
2016-01-15 11:15:13 +0100

    ajout fonction calcul d'une mensualité de lissage

--- bank_utils.py
+++ bank_utils.py
@@ -19,3 +19,57 @@ def cal_mensualite(C0, t, n):
     val_mens = num/denum
 
     return val_mens
+
+def cal_capital_enpruntable(m, t, n):
+    C_tot = m*(1-np.power(1+t/12, -1*n))
+    C_tot *= 12/t
+    C = cal_prix_hors_frais(C_tot)
+
+    return C
+
+def cal_frais_notaire(C):
+    C_tot = C * 1.076
+    C_tot += 1200
+    return
+
+def cal_prix_hors_frais(C_tot):
+    C = C_tot - 1200
+    C /= 1.076
+    return C
+
+def cal_mens_lisse(C0, mens_p1, nb_mens_p1, nb_mens_tot, t, t_p1):
+    """Calcul la  mensualite pour le lisse de 2 prets de taux potentiellements
+    different
+
+    Keyword arguments:
+    C0 -- Capital principal emprunte
+    mens_p1 -- Montant de la mensualite de pret p1
+    nb_mens_p1 -- Nombre de mensualite du pret p1
+    nb_mens_tot -- Nombre de mensualite du pret principal
+    t -- taux du pret principal
+    t_p1 -- taux du pret p1
+
+    return values:
+    val_mens_pp1 = valeur de la mensualite du pret principale sur la periode
+        nb_mens_p1
+    val_mens_lisse = valeur de la mensualite lisse
+    """
+    val_pw_num = -1.*nb_mens_p1
+    val_pw_denum = -1.*nb_mens_tot
+    #if (t_p1 == 0.):
+    #    num = C0 * t + mens_p1
+    #else :
+    num = C0 * t + mens_p1 * (1-np.power((1+t_p1), val_pw_num))
+    denum = 1-np.power(1+t, -1*nb_mens_tot)
+    print num
+    print denum
+    print C0
+    print mens_p1
+    print t_p1
+    print nb_mens_p1
+    print t
+    print nb_mens_tot
+    val_mens_lisse = num/denum
+    val_mens_pp1 = val_mens_lisse - mens_p1
+
+    return val_mens_lisse, val_mens_pp1
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from bank_utils import *
 taux_annuel = 4/100.
 capital_emprunte = 116519
 duree_tot = 16 * 12.
+
 val_amort_init = amortissement_init(capital_emprunte, taux_annuel, duree_tot)
 tab_amortissement = []
 val_mensualite=cal_mensualite(capital_emprunte, taux_annuel, duree_tot)
@@ -25,14 +26,22 @@ val_capital_restant=np.sum(tab_amortissement[108:192])
 print val_interet_restant
 print val_capital_restant
 
-mens_7_2_5 = cal_mensualite(val_capital_restant, 2.5/100., 7*12)
+mens_7_2_5 = cal_mensualite(val_capital_restant, 1.85/100., 7*12)
 
 print mens_7_2_5
 print "gain mensuel : " + str(val_mensualite-mens_7_2_5)
 print "Nb de mois gagnés : " + str((val_mensualite-mens_7_2_5)*7*12/val_mensualite)
-plt.figure()
-plt.plot(tab_amortissement)
-plt.plot(part_interet)
-plt.show()
+print "Mensualite 20, 2.5, 142000 : " + str(cal_mensualite(142000, 2.5/100., 240))
+# plt.figure()
+# plt.plot(tab_amortissement)
+# plt.plot(part_interet)
+# plt.show()
+print "Capital empruntable 950 : " + str(cal_capital_enpruntable(950, 2.55/100., 240))
+duree_tot = 20
+p1 = 4
+taux_annuel = 2.55/100.
+taux_annuel_p1 = 3.5/100.
+val_mens_fin, val_mens_debut = cal_mens_lisse(150000, 350, p1*12, duree_tot*12,
+                                              taux_annuel/12., taux_annuel_p1/12)
 
-
+print "Exemple de 175000 sur 20 ans avec un palier de 300 après " + str(p1) + " ans. début : " + str(val_mens_debut) + " Fin : " + str(val_mens_fin)
